b2b290a6b9491cc42c3b83fe6db9d0eaf71e74be
Viet Nguyen <viet@Viets-MacBook-Pro.local>
2022-11-09 18:41:19 -0500

    second attempt of scrapping deals off slick deals

--- slickdeals.py
+++ slickdeals.py
@@ -2,39 +2,27 @@ from bs4 import BeautifulSoup
 from requests import get
 
 def scrapeSlickDeals():
-  url = 'https://slickdeals.net/'
-  r = get(url)
-  soup = BeautifulSoup(r.text, 'html.parser')
+    url = 'https://slickdeals.net/'
+    r = get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-  # copied from https://github.com/MinweiShen/slickdeals/blob/master/slickdeals/utils.py
-  removeHidden = soup.find_all('div', {'class': 'removeHidden'})
-  fpdeals = []
-  for r in removeHidden:
-      fpdeals += r.find_all('div', {'class': 'frontpage'})
-  deals = []
+    removeHidden = soup.find_all('div', {'class': 'removeHidden'})
 
-  for d in fpdeals:
-      a = d.find('a', {'class': 'itemTitle'})
-      link = SLICKDEALS + a['href']
-      price_line = d.find('div', {'class': 'priceLine'})
-      title = price_line['title']
-      price_div = price_line.find('div', {'class': 'itemPrice'})
-      price = price_div and price_div.getText().strip() or 'None'
-      shipinfo_div = price_line.find('div', {'class': 'priceInfo'})
-      shipinfo = shipinfo_div and shipinfo_div.getText().strip() or 'None'
-      item = {
-          'title': title,
-          'link': link,
-          'price': price,
-          'info': shipinfo
-      }
-      if show_free_price:
-          if 'free' in price.lower():
-              deals.append(item)
-      elif show_free:
-          if 'free' in shipinfo.lower() or 'free' in price.lower():
-              deals.append(item)
-      else:
-          deals.append(item)
+    fpdeals = []
+    for r in removeHidden:
+        fpdeals += r.find_all('li', {'data-module-id': 'Frontpage Slickdeals'})
+    deals = []
 
-  return deals
+    for d in fpdeals:
+        card_content = d.find('div', {'class': 'bp-c-card_content'})
+        title = card_content.find('a', {'class': 'bp-c-card_title'})
+        price = card_content.find('span', {'class': 'bp-p-dealCard_price'})
+
+        item = {
+          'title': title.text,
+          'price': price.text if price else None,
+          'link': url + title['href'] if title['href'] else None,
+        }
+        deals.append(item)
+
+    return deals
